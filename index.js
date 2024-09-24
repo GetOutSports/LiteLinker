e0bcd29f0dd09ff82ad426c671da143a8be0ca7d
Michel Grolet <39418855+michelgrolet@users.noreply.github.com>
2024-09-24 12:52:22 +0200

    init

---
+++ index.js
@@ -0,0 +1,59 @@
+const goLinks = {
+	// settings
+	'add': 'https://github.com/MichelGrolet/go/edit/main/go-links.js',
+	'new': 'add',
+	'edit': 'add',
+	// socials
+	'insta': 'https://www.instagram.com/mi.grlt/',
+    'instagram': 'insta',
+	'linkedin': 'https://www.linkedin.com/in/michelgrolet/',
+	'ln': 'linkedin',
+	'bc': 'https://www.getout.sport/',
+};
+
+function redirect() {
+    const basePath = 'go'; // repository name
+    let path = window.location.pathname.substr(1); // get the part after the initial slash
+
+    if (path === 'links') return; // Prevent redirect on /links
+
+    if (path.startsWith(basePath)) {
+        path = path.substr(basePath.length + 1); // +1 to account for the slash
+    }
+
+    const redirectURL = goLinks[path];
+    if (redirectURL) {
+        window.location.href = redirectURL;
+    } else {
+        window.location.href = `https://getout.sport/404`;
+    }
+}
+
+function generateLinksPage() {
+    let html = '<!DOCTYPE html><html><head><title>Go Links</title></head><body>';
+    html += '<h1>Go Links</h1><ul>';
+
+    for (const [key, url] of Object.entries(goLinks)) {
+        html += `<li><a href="${url}">${key}</a></li>`;
+    }
+
+    html += '</ul></body></html>';
+    return html;
+}
+
+const http = require('http');
+
+http.createServer((req, res) => {
+    if (req.url === '/links') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(generateLinksPage());
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+    }
+}).listen(80, () => {
+    console.log('Server is running at http://go.getout.sport/');
+});
+
+// Call redirect function on page load
+redirect();
