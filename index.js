7c57094aa5233eb0b790a19ff60f62a10467a593
Michel Grolet <39418855+michelgrolet@users.noreply.github.com>
2024-10-30 10:55:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const goLinks = {
 	// business
 	'bc': 'https://www.getout.sport',
 	'write-a-review', 'https://g.page/r/CcxuwieS9tjcEBM/review',
+		'review': 'write-a-review',
 	// socials
 	'tiktok': 'https://www.tiktok.com/@getoutinc',
 	'instagram': 'https://www.instagram.com/getoutsport',
